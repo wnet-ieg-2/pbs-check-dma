33b1c4751375acc67c8f611cab3c3d932db6b6ac
William Tam <tamw@wnet.org>
2018-11-20 18:47:46 +0000

    refined endpoint template

--- templates/endpoint-template.php
+++ templates/endpoint-template.php
@@ -16,15 +16,11 @@ if (empty($_POST['media_id'])) {
   foreach ($apicheck['location'] as $key => $var) {
     $location_string .= "$key : $var  ";
   }
-
+  $location_string = !empty($location_string) ? $location_string : "a location that we are unable to determine";
   if ($apicheck[0]) {
     $media_id = $_POST['media_id'];
-    $return = "<div class='video-wrap'><!-- Your location appears to be $location_string DUE TO CONTRACTUAL OBLIGATIONS TO THE PRODUCERS, CAST, AND CREW OF THE PERFORMANCE RECORDED IN THE VIDEO BELOW, IT IS PROHIBITED TO PLAY THE VIDEO BELOW ON DEVICES THAT ARE NOT PHYSICALLY LOCATED WITHIN THE WNET/THIRTEEN BROADCAST AREA. THIS AREA IS COMPRISED OF THE FOLLOWING COUNTIES: NY: Bronx, Dutchess, Kings, Nassau, New York, Orange, Putnam, Queens, Richmond, Rockland, Suffolk, Sullivan, Ulster, Westchester;  NJ: Bergen, Essex, Hudson, Hunterdon, Middlesex, Monmouth, Morris, Ocean, Passaic, Somerset, Sussex, Union, Warren; CT: Fairfield; PA: Pike. PLEASE RESPECT THESE RIGHTS. --><iframe src='//player.pbs.org/widget/partnerplayer/$media_id/?chapterbar=false' frameborder='0' marginwidth='0' marginheight='0' scrolling='no' webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe></div>";
+    $return = "<div class='video-wrap'><!-- DUE TO CONTRACTUAL OBLIGATIONS TO THE PRODUCERS, CAST, AND CREW OF THE PERFORMANCE RECORDED IN THE VIDEO BELOW, IT IS PROHIBITED TO PLAY THE VIDEO BELOW ON DEVICES THAT ARE NOT PHYSICALLY LOCATED WITHIN THE WNET/THIRTEEN BROADCAST AREA. THIS AREA IS COMPRISED OF THE FOLLOWING COUNTIES: NY: Bronx, Dutchess, Kings, Nassau, New York, Orange, Putnam, Queens, Richmond, Rockland, Suffolk, Sullivan, Ulster, Westchester;  NJ: Bergen, Essex, Hudson, Hunterdon, Middlesex, Monmouth, Morris, Ocean, Passaic, Somerset, Sussex, Union, Warren; CT: Fairfield; PA: Pike. PLEASE RESPECT THESE RIGHTS. Your device appears to be located in $location_string --><iframe src='//player.pbs.org/widget/partnerplayer/$media_id/?chapterbar=false' frameborder='0' marginwidth='0' marginheight='0' scrolling='no' webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe></div>";
   } else {
-    $location_string = "";
-    foreach ($apicheck['location'] as $key => $var) {
-      $location_string .= "$key : $var  ";
-    }
     $return = "<div class='video-wrap'><div class='sorry'><h3>Sorry, this content is only available to viewers within our broadcast area*.</h3><p>Check your local PBS listings to find out where you can watch.</p><p>*Our broadcast area contains the following counties:  NY: Bronx, Dutchess, Kings, Nassau, New York, Orange, Putnam, Queens, Richmond, Rockland, Suffolk, Sullivan, Ulster, Westchester;  NJ: Bergen, Essex, Hudson, Hunterdon, Middlesex, Monmouth, Morris, Ocean, Passaic, Somerset, Sussex, Union, Warren; CT: Fairfield; PA: Pike.</p><p>Your device appears to be located in $location_string. If this is not correct, it may be due to VPN software on your device, or mis-reporting of your location by your network or cable provider.</p></div></div>";
   }
 }
