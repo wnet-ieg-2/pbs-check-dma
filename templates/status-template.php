300cf37e27e70b57c37f8828bbc97637ebed2bc2
William Tam <tamw@wnet.org>
2020-03-26 21:21:42 +0000

    integrating new blackout status metadata into livestream status endpoint

--- templates/status-template.php
+++ templates/status-template.php
@@ -7,45 +7,14 @@ remove_all_actions('wp_header',1);
 $api = new PBS_Check_DMA();
 $defaults = get_option($api->token);
 $station_common_name = !empty($defaults['station_common_name']) ? $defaults['station_common_name'] : "";
-
-
-$blackout_schedule = array(
-  "weekly" => array(
-    "monday" => array(
-      array(
-        "start" => "0730", "end" => "0800"
-      )
-    ),
-    "tuesday" => array(
-      array(
-        "start" => "0730", "end" => "0800"
-      )
-    ),
-    "wednesday" => array(
-      array(
-        "start" => "0730", "end" => "0800"
-      )
-    ),
-    "thursday" => array(
-      array(
-        "start" => "0730", "end" => "0800"
-      )
-    ),
-    "friday" => array(
-      array(
-        "start" => "0730", "end" => "0800"
-      )
-    )
-  ),
-  "dates" => array(
-    "2020-03-25" => array(
-      array(
-        "start" => 1700, "end" => 1800
-      )
-    )
-  )
-);
-
+$blackout_schedule = array();
+if (!empty($_POST['postid'])) {
+  $postmeta_data = get_post_meta($_POST['postid']);
+  $blackouts_weekly = !empty($postmeta_data['dma_restricted_video_blackouts_weekly'][0]) ? stripslashes($postmeta_data['dma_restricted_video_blackouts_weekly'][0]) : '';
+  $blackouts_dates = !empty($postmeta_data['dma_restricted_video_blackouts_dates'][0]) ? stripslashes($postmeta_data['dma_restricted_video_blackouts_dates'][0]) : '';
+  $blackout_schedule["weekly"] = json_decode($blackouts_weekly, true);
+  $blackout_schedule["dates"] = json_decode($blackouts_dates, true);
+}
 $tz = !empty(get_option('timezone_string')) ? get_option('timezone_string') : 'America/New York';
 $date = new DateTime('now', new DateTimeZone($tz));
 
@@ -53,30 +22,29 @@ $dayname = $date->format('D');
 $ymd = $date->format('Y-m-d');
 $miltime = $date->format('Hi');
 
-$blackout = false;
+$blackout_status = array(
+  "blackout_status" => false
+);
 
 foreach ($blackout_schedule as $type => $data) {
   foreach($data as $schedule => $times) {
-    error_log($type . " " . $schedule);
     if ($type == 'weekly') {
       if (strtolower($schedule) != strtolower($dayname)) {
         continue;
       }
     } else {
       if ($schedule != $ymd) {
-        error_log("schedule is $schedule which is not eq to $ymd");
         continue;
       }
     }
-    error_log("times is not empty:" . json_encode($times));
     if (!empty($times)) {
-      error_log("for reals");
       foreach ($times as $time => $pair) {
-        error_log(json_encode($pair));
-        error_log($miltime);
-        if ($miltime > $pair['start'] && $miltime < $pair['end']) {
-          $blackout = true;
-          error_log("blakcing out");
+        if ($miltime > (int) $pair['start'] && $miltime < (int) $pair['end']) {
+          $blackout_status["blackout_status"] = true;
+          $hours = substr($pair['end'], 0, 2);
+          $minutes = substr($pair['end'], 2, 2);
+          $end_time_str = date( 'g:i A', strtotime( $hours . ":" . $minutes ) );
+          $blackout_status["end"] = $end_time_str;
           break 2;
         }
       }
@@ -84,10 +52,6 @@ foreach ($blackout_schedule as $type => $data) {
   }
 }
 
-$blackout_status = array(
-  "blackout_status" => $blackout
-);
- 
 $return = $blackout_status;
 
 
