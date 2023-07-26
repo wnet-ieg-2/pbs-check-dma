2ca2bf48b490de8044426bb29ccf7da2c3371445
William Tam <tamw@wnet.org>
2023-07-26 14:51:07 +0000

    another fix

--- classes/class-pbs-check-dma.php
+++ classes/class-pbs-check-dma.php
@@ -325,7 +325,7 @@ class PBS_Check_DMA {
       if (!empty($postmeta['dma_restricted_video_image'][0])) {
         $mezz_image = $postmeta['dma_restricted_video_image'][0];
       }
-      $return = '<div class="dmarestrictedplayer program-player" data-media="' . $data_type . '" data-postid="' . $post_id . '"><img src="'.$mezz_image.'" /></div><link rel=stylesheet media="all" type="text/css" href="' . $this->assets_url . '"/css/pbs_check_dma.css?version=' . $this->version . ' />script src="' . $this->assets_url . '/js/pbs_check_dma.js?version=' . $this->version . '"></script>';
+      $return = '<div class="dmarestrictedplayer program-player" data-media="' . $data_type . '" data-postid="' . $post_id . '"><img src="'.$mezz_image.'" /></div><link rel=stylesheet media="all" type="text/css" href="' . $this->assets_url . '"/css/pbs_check_dma.css?version=' . $this->version . ' /><script src="' . $this->assets_url . '/js/pbs_check_dma.js?version=' . $this->version . '"></script>';
       if ($data_type ==  "custom_hls") {
         $return .= "<script src='$jwplayer_uri'></script>";
       } else {
